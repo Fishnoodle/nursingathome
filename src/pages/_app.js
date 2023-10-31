4c1cdd663c65522812647efa32d06024b3f78f04
Fishnoodle <shivar.vishal.pillay@gmail.com>
2023-10-31 12:21:37 -0700

    Initial backend with mongoose and bcrypt
    DB_URL not set. Awaiting user and password

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -56,6 +56,56 @@ import 'src/iconify-bundle/icons-bundle-react'
 // ** Global css styles
 import '../../styles/globals.css'
 
+// require database connection
+const dbConnect = require('./db/dbConnect')
+
+// execute database connection
+dbConnect()
+
+// bcrypt backend register endpoint
+const bcrypt = require('bcrypt')
+
+const User = require('./db/userModel')
+
+// register endpoint
+app.post('/register', (request, response) => {
+  // hash the password
+  bcrypt
+    .hash(request.body.password, 10)
+    .then(hashedPassword => {
+      // create a new user instance and collect the data
+      const user = new User({
+        email: request.body.email,
+        password: hashedPassword
+      })
+
+      // save the new user
+      user
+        .save()
+        // return success if the new user is added to the database successfully
+        .then(result => {
+          response.status(201).send({
+            message: 'User Created Successfully',
+            result
+          })
+        })
+        // catch error if the new user wasn't added successfully to the database
+        .catch(error => {
+          response.status(500).send({
+            message: 'Error creating user',
+            error
+          })
+        })
+    })
+    // catch error if the password hash isn't successful
+    .catch(e => {
+      response.status(500).send({
+        message: 'Password was not hashed successfully',
+        e
+      })
+    })
+})
+
 const clientSideEmotionCache = createEmotionCache()
 
 // ** Pace Loader
